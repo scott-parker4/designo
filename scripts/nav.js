08ae3e8b1279a106c100bd227e4215841b217244
Scott Parker <scottparkerdesign@gmail.com>
2021-07-09 12:58:39 -0400

    adds mobile nav link animation, and fixes mobile nav open animation

--- scripts/nav.js
+++ scripts/nav.js
@@ -4,7 +4,19 @@ const overlay = document.querySelector(".overlay")
 
 burger.addEventListener("click", showMenu)
 
+function vis() {
+  nav.classList.add("notVisible")
+  overlay.classList.add("notVisible")
+}
+
 function showMenu() {
+  if (nav.classList.contains("active")) {
+    setTimeout(vis, 300)
+  } else {
+    nav.classList.remove("notVisible")
+    overlay.classList.remove("notVisible")
+  }
+
   nav.classList.toggle("active")
   overlay.classList.toggle("active")
   burger.classList.toggle("is-active")
